5638b92caa6709650e084097b5db318c72d7213c
Methran <methran2003@gmail.com>
2025-08-08 02:25:42 +0100

    added signalr hub url in prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     apiBaseUrl: 'https://verbatim-api.up.railway.app/api',
+    hubUrl: 'https://verbatim-api.up.railway.app/competitive-hub'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     // apiBaseUrl: 'https://verbatim-api.up.railway.app/api',
-    apiBaseUrl: 'http://localhost:5079/api'
+    apiBaseUrl: 'http://localhost:5079/api',
+    hubUrl: 'http://localhost:5079/competitive-hub'
 };
